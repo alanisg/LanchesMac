4b2433beafdf0e16f44037d9ca4a01541ee4815a
Gabriel <Gabriel>
2022-09-30 20:39:07 -0300

    Aulas 30 e 31

---
+++ LanchesMac/Controllers/LancheController.cs
@@ -0,0 +1,20 @@
+﻿using LanchesMac.Repositories.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILanchesRepository _lanchesRepository;
+        public LancheController(ILanchesRepository lanchesRepository)
+        {
+            _lanchesRepository = lanchesRepository;
+        }
+
+        public IActionResult List()
+        {
+            var lanche = _lanchesRepository.Lanches;
+            return View(lanche);
+        }
+    }
+}
